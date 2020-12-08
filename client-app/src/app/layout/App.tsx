ff68a9c4f3508fce93097ab64b83cac06e3233ba
D4V1DD33P <201404972@post.au.dk>
2020-12-08 07:54:55 +0100

    Frontend src import

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+import { Container } from 'semantic-ui-react'
+import { observer } from 'mobx-react-lite';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import NavBar from '../../features/nav/NavBar';
+import { Route, RouteComponentProps, withRouter } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import ActivityForm from '../../features/activities/form/ActivityForm';
+
+const App: React.FC<RouteComponentProps> = ({ location }) => {
+  return (
+    <Fragment>
+      <Route exact path='/' component={HomePage} />
+      <Route path={'/(.+)'} render={() => (
+        <Fragment>
+          <NavBar />
+          <Container style={{ marginTop: '7em' }}>
+            <Route exact path='/activities' component={ActivityDashboard} />
+            <Route path='/activities/:id' component={ActivityDetails} />
+            <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+          </Container>
+        </Fragment>
+      )} />
+
+    </Fragment>
+  );
+}
+
+export default withRouter(observer(App));
