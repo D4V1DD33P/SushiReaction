cfdb2eacb83a92802977c0c50bc1b168bb0a157b
D4V1DD33P <201404972@post.au.dk>
2020-12-13 15:55:40 +0100

    Refracturing to use ChakraUI

--- client-app/src/features/sushis/dashboard/SushiListItem.tsx
+++ client-app/src/features/sushis/dashboard/SushiListItem.tsx
@@ -1,33 +1,48 @@
-import { observer } from 'mobx-react-lite'
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { Item, Button, Label } from 'semantic-ui-react'
-import { ISushi } from '../../../app/models/sushi'
-import SushiStore from '../../../app/stores/sushiStore'
+import { StarIcon } from "@chakra-ui/icons";
+import { Badge, Box, Image } from "@chakra-ui/react";
+import { observer } from "mobx-react-lite";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Item, Button, Label } from "semantic-ui-react";
+import { ISushi } from "../../../app/models/sushi";
+import SushiStore from "../../../app/stores/sushiStore";
 
 export const SushiListItem: React.FC<{ sushi: ISushi }> = ({ sushi }) => {
-    const sushiStore = useContext(SushiStore);
-    const { deleteSushi, submitting, target } = sushiStore;
-    return (
-        <Item key={sushi.id}>
-            <Item.Content>
-                <Item.Header as='a'>{sushi?.number} - {sushi.name}</Item.Header>
-                <Item.Description>
-                    <div>{sushi.description}</div>
-                    <div>
-                        {sushi.category}
-                    </div>
-                </Item.Description>
-                <Item.Extra>
-                    <Button
-                        as={Link} to={`/sushis/${sushi.id}`}
-                        floated='right'
-                        content='View'
-                        color='brown'
-                    />
-                    <Label basic content={sushi.category} />
-                </Item.Extra>
-            </Item.Content>
-        </Item>
-    )
-}
+  const sushiStore = useContext(SushiStore);
+  const { submitting, target } = sushiStore;
+  return (
+    <Box maxW="xs" borderWidth="1px" borderRadius="lg" overflow="hidden">
+      <Image src={`/assets/categoryImages/${sushi.category}.png`} />
+      <Box p="3">
+        <Box d="flex" alignItems="baseline">
+          <Box
+            fontWeight="semibold"
+            letterSpacing="wide"
+            fontSize="sm"
+            textTransform="uppercase"
+          >
+            {sushi.number} &bull; {sushi.name}
+          </Box>
+
+          <Badge borderRadius="full" px="2" colorScheme="teal" ml="1">
+            {sushi.category}
+          </Badge>
+        </Box>
+          <Box as="span" color="gray.600" fontSize="sm">
+            {sushi.description}
+          </Box>
+
+        <Box d="flex" mt="2" alignItems="center">
+          {Array(5)
+            .fill("")
+            .map((_, i) => (
+              <StarIcon key={i} color={i < sushi.rating ? "yellow.500" : "yellow.300"} />
+            ))}
+          <Box as="span" ml="2" color="gray.600" fontSize="sm">
+            Rate Me
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
